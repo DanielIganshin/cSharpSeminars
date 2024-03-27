9a8c9142453243d177ff33bee990c11f2bec6c3e
DanilIganshin <iganshindanil@gmail.com>
2024-03-27 03:03:52 +0300

    make all hometasks

--- homework/Program.cs
+++ homework/Program.cs
@@ -116,3 +116,42 @@
 //         CheckIfEven(number);
 //     }
 // }
+using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+  
+
+
+int N = 2;
+
+if(number > 1)
+{
+    while(N <= number)
+    {
+        Console.Write(N + " ");
+        N = N + 2;
+    }
+}
+
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 9;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
--- lesson1/obj/Debug/net8.0/lesson1.AssemblyInfo.cs
+++ lesson1/obj/Debug/net8.0/lesson1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5230ee1b4674b9e0ef49877739aa6129430387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd1c0845ec26a7706d8cba2e7aafeb843acbaa3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lesson1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
